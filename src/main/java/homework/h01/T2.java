d86d1de49a1a3f029f09d1fd07101a806273e82f
Volodymyr Blahuta <blahutavolodymyr@e-u.edu.ua>
2026-09-24 12:41:02 +0300

    Add Solution class with countOdds method

--- src/main/java/homework/h01/T2.java
+++ src/main/java/homework/h01/T2.java
@@ -3,3 +3,9 @@ package homework.h01;
 // advanced
 // https://leetcode.com/problems/rectangle-area/
 public class T2 {}
+class Solution {
+    public int countOdds(int low, int high) {
+        return (high + 1) / 2 - low / 2;
+    }
+}
+
