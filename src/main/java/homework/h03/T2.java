87a932c3395672a3c062989b6c0cfacb6fd568aa
Volodymyr Blahuta <blahutavolodymyr@e-u.edu.ua>
2026-09-24 14:05:48 +0300

    Implement differenceOfSums method in Solution class

--- src/main/java/homework/h03/T2.java
+++ src/main/java/homework/h03/T2.java
@@ -3,3 +3,13 @@ package homework.h03;
 // advanced
 // https://leetcode.com/problems/reverse-integer/
 public class T2 {}
+class Solution {
+    public int differenceOfSums(int n, int m) {
+        int result = 0;
+        for (int i = 1; i <= n; i++) {
+            if (i % m == 0) result -= i;
+            else result += i;
+        }
+        return result;
+    }
+}
