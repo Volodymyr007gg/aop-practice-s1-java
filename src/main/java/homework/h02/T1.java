602380a17aacfc5be391e99f552e964d5668ebb0
Volodymyr Blahuta <blahutavolodymyr@e-u.edu.ua>
2026-09-24 12:49:45 +0300

    Implement addDigits method in Solution class

--- src/main/java/homework/h02/T1.java
+++ src/main/java/homework/h02/T1.java
@@ -3,3 +3,9 @@ package homework.h02;
 // base
 // https://leetcode.com/problems/add-binary/
 public class T1 {}
+class Solution {
+    public int addDigits(int num) {
+        if (num == 0) return 0;
+        if (num % 9 == 0) return 9;
+        return num % 9;
+    }
