3a1180812a81f1b81d160b6381c5b64b795037d0
Volodymyr Blahuta <blahutavolodymyr@e-u.edu.ua>
2026-09-24 12:40:28 +0300

    Implement smallestEvenMultiple method in Solution class

--- src/main/java/homework/h01/T1.java
+++ src/main/java/homework/h01/T1.java
@@ -3,3 +3,11 @@ package homework.h01;
 // base
 // https://leetcode.com/problems/palindrome-number/
 public class T1 {}
+class Solution {
+    public int smallestEvenMultiple(int n) {
+        if (n % 2 == 0) {
+            return n;
+        }
+        return n * 2;
+    }
+}
